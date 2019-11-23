48a5bfff58a680c42bfbb2c74d85c3d706d682ca
hpark95 <parkhs1995@gmail.com>
2019-11-23 14:01:18 -0500

    feat: freq_labels.txt added. It contains the ImageNet class prediction of whole dataset using ResNext101, a model with the highest top-1 accuracy among PyTorch pre-trained models.

--- filter_data.py
+++ filter_data.py
@@ -1,3 +1,7 @@
+# TODO: Refactor
+# TODO: Check if CUDA works correctly
+# TOOO: Make a function to filter data
+
 import numpy as np
 import torch
 import torchvision
@@ -6,7 +10,6 @@ import os
 from PIL import Image
 import regex as re
 import torch.utils.data as data
-from torch.autograd import Variable
 
 data_path = 'dataset'
 test_img_filename = 'agkistrodon-contortrix/0a0c57ca18.jpg'
@@ -20,7 +23,7 @@ with open("ImageNetLabels.txt") as f:
         ImageNetLabels[int(k)] = v
 
 
-def get_frequent_image_classes(data_path):
+def get_frequent_image_classes(data_path, batch_size=256):
     freq = np.zeros(1000)
 
     model = torchvision.models.resnext101_32x8d(pretrained=True)
@@ -34,25 +37,24 @@ def get_frequent_image_classes(data_path):
             std=[0.229, 0.224, 0.225]
         )])
 
+    model.eval()
     if torch.cuda.is_available():
         print("using GPU")
-        model.cuda()
-    model.eval()
-    # count = 0
+        model.to(torch.device("cuda"))
     dataset = torchvision.datasets.ImageFolder(root=data_path, transform=transform)
 
-    full_data = data.DataLoader(dataset, batch_size=1, shuffle=False)
+    full_data = data.DataLoader(dataset, batch_size=batch_size, shuffle=False)
 
     with torch.no_grad():
-        for i, (img, label) in enumerate(full_data):
-            if i > 10:
-                break
-            # if i % 100 == 0:
-            #     print(str(i) + ' images evaluated.')
-            #img = torch.unsqueeze(img, 0)
-            out = model(img)
-            out = torch.argmax(out, 1)
-            freq[out] += 1
+        for i, (batch_img, _) in enumerate(full_data):
+            if i % 10 == 0:
+                print(str(batch_size*i) + ' images evaluated.')
+            if torch.cuda.is_available():
+                batch_img = batch_img.to(torch.device("cuda"))
+            batch_out = model(batch_img)
+            batch_out = torch.argmax(batch_out, 1)
+            for _, out in enumerate(batch_out):
+                freq[out] += 1
 
 
     print(freq)
@@ -67,23 +69,23 @@ def get_frequent_image_classes(data_path):
 
 
 if __name__ == '__main__':
-    model = torchvision.models.resnext101_32x8d(pretrained=True)
-
-    transform = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225]
-        )])
-
-    img = Image.open(os.path.join(data_path, test_img_filename))
-    img = transform(img)
-    img = torch.unsqueeze(img, 0)
-    model.eval()
-    out = model(img)
-    out = torch.Tensor.flatten(out)
-    _, classes = torch.sort(out, descending=True)
-    print(classes[:5])
+    # model = torchvision.models.resnext101_32x8d(pretrained=True)
+    #
+    # transform = transforms.Compose([
+    #     transforms.Resize(256),
+    #     transforms.CenterCrop(224),
+    #     transforms.ToTensor(),
+    #     transforms.Normalize(
+    #         mean=[0.485, 0.456, 0.406],
+    #         std=[0.229, 0.224, 0.225]
+    #     )])
+    #
+    # img = Image.open(os.path.join(data_path, test_img_filename))
+    # img = transform(img)
+    # img = torch.unsqueeze(img, 0)
+    # model.eval()
+    # out = model(img)
+    # out = torch.Tensor.flatten(out)
+    # _, classes = torch.sort(out, descending=True)
+    # print(classes[:5])
     get_frequent_image_classes(data_path=data_path)
